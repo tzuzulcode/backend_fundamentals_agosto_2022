46eb368272a50512a9dedcba296965c193935b67
Tzuzul <mail@tzuzulcode.com>
2022-08-22 08:11:35 -0500

    Status codes

--- ProyectoEjemplo/index.js
+++ ProyectoEjemplo/index.js
@@ -9,6 +9,19 @@ app.get("/",function(peticion,respuesta){
     return respuesta.send("<h1>Hola mundo</h1>")
 })
 
+app.post("/saludar",function(peticion,respuesta){
+    console.log(peticion.body)
+
+    return respuesta.status(201).send("<h1>Hola</h1>")
+})
+
+// app.put()
+
+// app.delete()
+
 app.listen(4000,function(){
     console.log("Listening on: http://localhost:4000")
 }) // callbacks
+
+
+//Actividad: Investigar acerca de: form-data, x-www-form-urlencoded, application/json
