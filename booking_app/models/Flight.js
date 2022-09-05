dcb2d0621d508d0ac4269d02d7768bd2df371a21
Ulver <lorito.free@gmail.com>
2022-09-04 21:31:18 -0400

    Agregado el fetch con then y catch, además de create Flights en Json con su respectiva query

--- booking_app/models/Flight.js
+++ booking_app/models/Flight.js
@@ -1,20 +1,33 @@
-const { query } = require("../libs/db")
+const { query } = require('../libs/db')
 
-class Flight{
-    // Static -> No es necesario instanciar la clase para utilizarse
-    static getAll(){
-        // const flights = await query("SELECT * FROM flight") // Espera a completar, pero en pause
-        // // Pending:
+class Flight {
+  // Static -> No es necesario instanciar la clase para utilizarse
+  static getAll() {
+    // const flights = await query("SELECT * FROM flight") // Espera a completar, pero en pause
+    // // Pending:
 
-        // return flights
+    // return flights
 
-        return query("SELECT * FROM flights")
+    return query('SELECT * FROM flights')
+  }
+
+  static async create(data) {
+    try {
+      const result = await query('INSERT INTO flights(??) VALUES(?)', [
+        Object.keys(data),
+        Object.values(data),
+      ])
+
+      return {
+        flight: data,
+        success: true,
+        message: 'Vuelo registrado correctamente',
+      }
+    } catch (error) {
+      console.log(error)
+      return error
     }
-
-    static create(data){
-        // Crear una query
-        // INSERT INTO
-    }
+  }
 }
 
 module.exports = Flight
